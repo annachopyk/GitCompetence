9947329e93ec63341497e93c9e842bb175115e1f
annachopyk <annachopuk0@gmail.com>
2022-10-17 21:57:53 +0300

    Find Factorial of a number using for loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    public static void findFactorial() {
+        int num = 10;
+        long factorial = 1;
+        for (int i = 1; i <= num; ++i) {
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        findFactorial();
     }
 }
